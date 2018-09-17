44affd27c81093fc7df0e32fa0420348e391247b
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-09-17 03:01:44 -0400

    Fixed Integration test script

--- ci/scripts/integration-test.sh
+++ ci/scripts/integration-test.sh
@@ -3,7 +3,7 @@
 set -xe
 
 export NEXT_APP_COLOUR=$(cat ./app-info/next-deployment.txt)
-export NEXT_APP_URL=http://$CF_APP-$NEXT_APP_COLOUR.$CF_APP_DOMAIN/
+export NEXT_APP_URL=http://$CF_APP-$NEXT_APP_COLOUR.$CF_APP_DOMAIN/pcf
 
 echo "Running integration test..."
 
